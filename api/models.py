e887d5ce5394abb56485c16ea0c7d022c6edc76c
BekpolatNormurodov <bekpolatdev1995@gmail.com>
2023-11-04 20:07:25 +0500

    friendId

--- api/models.py
+++ api/models.py
@@ -34,7 +34,7 @@ class Cards(models.Model):
     
 class Friends(models.Model):
     name = models.CharField(max_length=100, null=True)
-    friendId = models.IntegerField(null=True)
+    friendId = models.CharField(max_length=100, null=True)
     userId = models.ForeignKey(Users, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
