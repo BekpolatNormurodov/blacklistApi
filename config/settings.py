5ba4f30e7ca61f69b4399c8624a63c030f1f73e1
BekpolatNormurodov <bekpolatdev1995@gmail.com>
2023-12-03 12:28:53 +0500

    d

--- config/settings.py
+++ config/settings.py
@@ -105,9 +105,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
+
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+STATICFILES_DIRS=[str(BASE_DIR) + "/assets",]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
