88101981be41324adac15eb8d9156469a9a906e6
BekpolatNormurodov <bekpolatdev1995@gmail.com>
2023-12-04 16:09:03 +0500

    d wdd

--- api/serializers.py
+++ api/serializers.py
@@ -3,18 +3,18 @@ from .models import Users, Phones, Cards, Friends, Workers
 from django.conf import settings
 
 class UsersSerializer(serializers.ModelSerializer):
-    image = serializers.SerializerMethodField()
+    profile_img = serializers.SerializerMethodField()
 
     class Meta:
         model = Users
         fields = ('id', 'image', 'name', 'surname', 'fatherName', 'birthday', 'isCourt', 'isCar', 'about', 'passportNumber', 'idNumber', 'username')
 
-    def get_image(self, obj):
+    def get_profile_img(self, obj):
         # Assuming 'image' is a FileField or ImageField in your model
         if obj.image:
             # Modify the image URL by appending the desired text
             image_url = f"{settings.MEDIA_URL}{obj.image}"
-            modified_url = f"{settings.BASE_URL}/blacklistApi{image_url}"
+            modified_url = f"{settings.BASE_URL}/blacklistApi/{image_url}"
             return modified_url
         return None
 
