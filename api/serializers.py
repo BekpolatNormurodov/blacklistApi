a9d8cf740d67dfdd426a81a19495e2cb2797a074
BekpolatNormurodov <bekpolatdev1995@gmail.com>
2023-12-04 16:22:51 +0500

    dwd

--- api/serializers.py
+++ api/serializers.py
@@ -3,18 +3,19 @@ from .models import Users, Phones, Cards, Friends, Workers
 from django.conf import settings
 
 class UsersSerializer(serializers.ModelSerializer):
-    image = serializers.SerializerMethodField()
+    image_url = serializers.SerializerMethodField(required=False)
 
     class Meta:
         model = Users
-        fields = ('id', 'image', 'name', 'surname', 'fatherName', 'birthday', 'isCourt', 'isCar', 'about', 'passportNumber', 'idNumber', 'username')
+        fields = ('id', 'image','image_url', 'name', 'surname', 'fatherName', 'birthday', 'isCourt', 'isCar', 'about', 'passportNumber', 'idNumber', 'username')
 
-    def get_image(self, obj):
+    def get_image_url(self, obj):
         if obj.image:
             image_url = f"{settings.MEDIA_URL}{obj.image}"
             modified_url = f"{settings.BASE_URL}/blacklistApi{image_url}"
             return modified_url
         return None
+
 
 
 
