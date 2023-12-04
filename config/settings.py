38e95d97b9b52d853a18a8237e1befddb4e19903
BekpolatNormurodov <bekpolatdev1995@gmail.com>
2023-12-04 15:03:37 +0500

    Ddd

--- config/settings.py
+++ config/settings.py
@@ -111,4 +111,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'files/static')
 MEDIA_URL = '/blacklistApi/files/uploads/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '/blacklistApi/files/uploads')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'blacklistApi/files/uploads')
