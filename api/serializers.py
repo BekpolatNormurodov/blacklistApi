0d7b81bff0330b38c56a3649bf73cf591d37fd8b
BekpolatNormurodov <bekpolatdev1995@gmail.com>
2025-12-08 16:12:28 +0500

    uzgartirildi

--- api/serializers.py
+++ api/serializers.py
@@ -3,36 +3,37 @@ from .models import *
 from django.conf import settings
 
 class UsersSerializer(serializers.ModelSerializer):
-    image_url = serializers.SerializerMethodField(required=False)
-
     class Meta:
         model = Users
-        fields = ('id', 'image','image_url', 'name', 'surname', 'fatherName', 'birthday', 'isCourt', 'isCar', 'about', 'passportNumber', 'idNumber', 'username')
-
-    def get_image_url(self, obj):
-        if obj.image:
-            image_url = f"{settings.MEDIA_URL}{obj.image}"
-            modified_url = f"{settings.BASE_URL}/blacklistApi{image_url}"
-            return modified_url
-        return None
-
-
+        fields = ('id', 'name', 'surname', 'username', 'birthday', 'passport_number', 'id_number')
 
 
 class PhonesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Phones
-        fields = ('id', 'number', 'userId')
+        fields = ('id', 'number', 'user')
 
-class CardsSerializer(serializers.ModelSerializer): 
+class CardsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cards
-        fields = ('id', 'bank', 'phone', 'number', 'date', 'fullname', 'userId')
+        fields = ('id', 'bank', 'number', 'expiry_date', 'user')
 
 class FriendsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Friends
-        fields = ('id', 'image', 'fullname', 'index', 'userId')
+        fields = ('id', 'fullname', 'user')
+
+class PhonesDetailSerializer(serializers.ModelSerializer):
+    user = UsersSerializer(read_only=True)
+    cards = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Phones
+        fields = ('id', 'number', 'user', 'cards')
+    
+    def get_cards(self, obj):
+        cards = Cards.objects.filter(user=obj.user)
+        return CardsSerializer(cards, many=True).data
 
 class WorkersSerializer(serializers.ModelSerializer):
     class Meta:
